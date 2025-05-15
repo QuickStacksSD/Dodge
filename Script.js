547c2cfb9aa42ce4e13cb5dc420e8a89c013818c
QuickStacksSD <quickstacksSD@gmail.com>
2025-05-14 21:56:54 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -129,7 +129,7 @@ function gameLoop() {
     localStorage.setItem("finalScore", finalScore);
   
     // Redirect to game over page
-    window.location.href = "GameOver.html";
+    window.location.href = "../GameOver.html";
   
     return; // stop the game loop or any further code
   }
